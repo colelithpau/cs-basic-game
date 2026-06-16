808566e4e8e00a3e9b29bf1fed9c3e6568947313
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:54:53 +0800

    Create level19.py

---
+++ level19.py
@@ -0,0 +1,8 @@
+# There may be something around to help you!
+hero.moveUp()
+hero.moveRight(2)
+hero.moveDown(2)
+hero.attack("Cupboard")
+# First, move to the Cupboard.
+
+# Then, attack the "Cupboard" inside a while-true loop.
