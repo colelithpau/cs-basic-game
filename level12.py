797ffd9a124543fdd4ac60ef68f16551f9319a3f
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:48:56 +0800

    Create level12.py

---
+++ level12.py
@@ -0,0 +1,12 @@
+# Free the prisoner, defeat the guard and grab the gem.
+hero.moveRight()
+hero.attack("Weak Door")
+
+# Defeat the guard, named "Two".
+hero.moveRight(2)
+hero.moveDown(3)
+hero.attack("Two")
+hero.attack("Two")
+hero.moveRight(2)
+
+# Get the gem.
