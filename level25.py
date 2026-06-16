f2480f0a8ccd4e47e0615b541da30508b083eecd
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 01:01:34 +0800

    Create level25.py

---
+++ level25.py
@@ -0,0 +1,15 @@
+hero.moveDown()
+while True:
+    hero.buildXY("fence", 36, 30) 
+    hero.moveRight()
+    hero.buildXY("fence", 49, 30)  
+    hero.moveRight()
+    hero.buildXY("fence", 59, 30)  
+    hero.moveRight()
+    hero.buildXY("fence", 71, 30)
+    hero.moveRight()
+    
+    
+
+    
+
