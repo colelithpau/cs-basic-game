5e596c5dd41735b251b769945aa4e0c964a086c4
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:45:47 +0800

    Add hero actions for COMBO challenge level
    Implement hero movement and attack sequence for level 9.

---
+++ level9.py
@@ -0,0 +1,23 @@
+# This is a COMBO challenge level.
+# Defeat ogres, collect gems, and escape to the red X
+# using strings and movement arguments!
+
+hero.moveDown()
+hero.moveRight(2)
+hero.moveUp()
+hero.attack("Ursa")
+hero.attack("Ursa")
+hero.moveLeft(2)
+hero.moveUp()
+hero.attack("Rexxar")
+hero.attack("Rexxar")
+hero.moveRight()
+hero.attack("Brack")
+hero.attack("Brack")
+hero.moveRight(3)
+
+
+
+
+
+
