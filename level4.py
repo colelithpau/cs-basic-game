24679996223dcd8059a066d5b8dba885551931c4
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:39:34 +0800

    Add hero movement logic for gem collection
    Implement hero movement to collect gems and exit.

---
+++ level4.py
@@ -0,0 +1,21 @@
+# This is a CONCEPT challenge about basic syntax.
+# Collect all gems and return to the exit (the red X).
+# Avoid the spikes.
+hero.moveUp()
+hero.moveRight()
+hero.moveDown(2)
+hero.moveUp()
+hero.moveRight()
+hero.moveLeft()
+hero.moveUp()
+hero.moveLeft()
+hero.moveDown()
+hero.moveLeft()
+
+
+
+
+
+
+
+
