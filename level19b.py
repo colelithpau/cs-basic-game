350295bca32110b2c5f2598663484157d8c4a0d7
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:56:02 +0800

    Create level19b.py

---
+++ level19b.py
@@ -0,0 +1,9 @@
+while True:
+    hero.moveRight()
+    hero.moveDown()
+    hero.moveRight()
+    hero.moveDown(2)
+    hero.attack("Cupboard")
+# First, move to the Cupboard.
+
+# Then, attack the "Cupboard" inside a while-true loop.
