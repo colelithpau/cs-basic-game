12f4628c8449b75a4a2f87058cdf564a3c76875b
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:53:14 +0800

    Add hero movement logic in level16.py
    Implemented a loop for hero movement to collect gems.

---
+++ level16.py
@@ -0,0 +1,12 @@
+# Collect all gems and escape at the white X mark.
+# Use no more than 5 statements.
+while True:
+    hero.moveUp(2)
+    hero.moveRight(2)
+    hero.moveDown()
+    hero.moveLeft()
+    
+
+
+
+
