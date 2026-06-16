e7213981f88229b53990a38a352bd9ebc6ef767b
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-17 00:59:14 +0800

    Correct hero attack logic for multiple enemies
    Fixed the code to ensure the hero attacks each enemy correctly.

---
+++ level22.py
@@ -0,0 +1,14 @@
+# There is a mistake in the code below.
+# Try to figure out what it is, then fix it!
+
+enemy1 = hero.findNearestEnemy()
+hero.attack(enemy1)
+hero.attack(enemy1)
+
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+enemy3 = hero.findNearestEnemy()
+hero.attack(enemy3)
+hero.attack(enemy3)
